600e90f544c70745e99ec6817e1f584e7aec62d4
unknown <caiocunha08@gmail.com>
2025-05-28 00:57:27 -0300

    funciona pfv

--- src/components/CardGrowthSemParar.tsx
+++ src/components/CardGrowthSemParar.tsx
@@ -1,5 +1,5 @@
 import { Card, CardHeader, CardDescription, CardTitle, CardFooter } from "./ui/card";
-import { fetchMetrics } from "@/pages/home/HomePage.tsx";
+import { fetchMetrics } from "@/pages/home/HomePage";
 import { Badge } from "./ui/badge";
 import { TrendingDownIcon, TrendingUpIcon } from "lucide-react";
 import { useTokenStore } from "@/hooks/useTokenStore";
